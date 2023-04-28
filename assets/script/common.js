b4eaf71a0a1c7fb87cc8c4e7c1fede08031821f0
hayeong <hychoi318@gmail.com>
2023-04-28 18:33:49 +0900

    서브페이지 추가

---
+++ assets/script/common.js
@@ -0,0 +1,31 @@
+$(function() {
+    // header
+    $('.gnb').on('mouseenter', function() {
+        $('#header').addClass('hover');
+    })
+    $('#header').on('mouseleave', function() {
+        $('#header').removeClass('hover');
+        $('.gnb__left > li').removeClass('active');
+        $('.gnb__right > li').removeClass('active');
+        $('.lnb__left > ul').removeClass('active');
+        $('.lnb__right > ul').removeClass('active');
+    })
+    $('.gnb__left > li, .lnb__left > ul').on('mouseenter', function() {
+        let index = $(this).index();
+        $('.gnb__left > li').eq(index).addClass('active').siblings().removeClass('active');
+        $('.lnb__left > ul').eq(index).addClass('active').siblings().removeClass('active');
+    })
+    $('.gnb__left > li, .lnb__left > ul').on('mouseleave', function() {
+        $('.gnb__left > li').removeClass('active');
+        $('.lnb__left > ul').removeClass('active');
+    })
+    $('.gnb__right > li, .lnb__right > ul').on('mouseenter', function() {
+        let index = $(this).index();
+        $('.gnb__right > li').eq(index).addClass('active').siblings().removeClass('active');
+        $('.lnb__right > ul').eq(index).addClass('active').siblings().removeClass('active');
+    })
+    $('.gnb__right > li, .lnb__right > ul').on('mouseleave', function() {
+        $('.gnb__right > li').removeClass('active');
+        $('.lnb__right > ul').removeClass('active');
+    })
+})
